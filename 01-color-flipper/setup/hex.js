d0cf9c5f68a429bdf3775a1db3840e0cb674b03d
BirdyArt <asobolev91@gmail.com>
2021-05-01 12:03:32 -0700

    first challenge solved

--- 01-color-flipper/setup/hex.js
+++ 01-color-flipper/setup/hex.js
@@ -1,1 +1,18 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+btn.addEventListener('click', () => {
+
+  document.body.style.backgroundColor = getRandomHex();
+  color.textContent = getRandomHex();
+});
+
+const getRandomHex = () => {
+  let myhex = '#';
+  for (i = 0; i < 6; i++) {
+    myhex += hex[Math.floor(Math.random() * 16)];
+  }
+  return myhex;
+}
