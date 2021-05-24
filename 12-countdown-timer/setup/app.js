456f03ca7b0fcb66af601b5cb5f446a7dd6d4db2
BirdyArt <asobolev91@gmail.com>
2021-05-24 12:04:42 -0700

    12th one solved

--- 12-countdown-timer/setup/app.js
+++ 12-countdown-timer/setup/app.js
@@ -21,3 +21,52 @@ const weekdays = [
   "Friday",
   "Saturday",
 ];
+
+const giveaway = document.querySelector('.giveaway');
+const deadline = document.querySelector('.deadline');
+const items = document.querySelectorAll('.deadline-format h4');
+
+let futureDate = new Date(2020,5,30,11,1,0);
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+const monthsNum = futureDate.getMonth();
+const weekdayNum = futureDate.getDay();
+
+
+giveaway.textContent = `giveaway ends on ${weekdays[weekdayNum]} ${months[monthsNum]} ${year} ${hours}:${minutes}am`;
+
+const futureTime = futureDate.getTime();
+
+const getRemainingTime = () => {
+  const today = new Date().getTime();
+  const t = futureTime - today;
+
+  const oneDay = 24*60*60*1000;
+  const oneHour = 60*60*1000;
+  const oneMinute = 60 *1000;
+
+  let days = Math.floor(t / oneDay);
+  let hours = Math.floor((t % oneDay) / oneHour);
+  let minutes = Math.floor((t % oneHour) / oneMinute);
+  let seconds = Math.floor((t % oneMinute) / 1000);
+
+  const values = [days, hours, minutes, seconds];
+
+  const format = (item) => {
+    return item < 10 ? item = `0${item}` : item; 
+  }
+
+  items.forEach((item, index) => {
+    item.innerHTML = format(values[index]);
+  })
+
+  if (t < 0) {
+    clearInterval(countdown);
+    deadline.innerHTML = `<h4 class="expired">sorry, this promo expired</h4>`
+  }
+
+}
+let countdown = setInterval(getRemainingTime, 1000);
+
+getRemainingTime();
